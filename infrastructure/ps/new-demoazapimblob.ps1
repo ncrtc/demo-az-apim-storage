f322997403a8c93edcaa2b5db8c92aa3861c92df
Tim Sullivan <timothyj.sullivan1@gmail.com>
2020-06-16 22:54:02 +0000

    powershell script updated, but policy doesn't apply

--- infrastructure/ps/new-demoazapimblob.ps1
+++ infrastructure/ps/new-demoazapimblob.ps1
@@ -8,26 +8,30 @@ param(
 
 $rgName = "rg-" + $name
 
-if (!(Get-AzureRmResourceGroup -Name $rgName)) {
+try {
+    Get-AzureRmResourceGroup -Name $rgName -ErrorAction Stop
+} catch {
     New-AzResourceGroup -Name $rgName -Location $location
 }
 
 $storName = "stor" + $name
-$containerName = "apimBlobs"
+$containerName = "apimblobs"
 Add-Content "test.txt" -Value "Hello World"
 New-AzStorageAccount -ResourceGroupName $rgname -Name $storName -location $location -skuname Standard_GRS
-
-$storContext = New-AzStorageContext -StorageAccountName $storName -UseConnectedAccount
-New-AzStorageContainer -Name $containerName -Context $storContext
-Set-AzStorageBlobContent -Container $containerName -File ".\test.txt" -Blob "test.txt"
 $storKey = Get-AzStorageAccountKey -name $storName -ResourceGroupName $rgName | select -First 1 -ExpandProperty Value
 
+$storContext = New-AzStorageContext -StorageAccountName $storName -StorageAccountKey $storKey
+New-AzStorageContainer -Name $containerName -Context $storContext
+Set-AzStorageBlobContent -Container $containerName -File ".\test.txt" -Blob "test.txt"  -Context $storContext
+
 $apimName = "apim-" + $name
-$apiId = "blob"
+$apiName = "blob"
 $Policy = (Invoke-WebRequest https://raw.githubusercontent.com/ncrtc/demo-az-apim-storage/master/policy-standard.xml).content.Replace("YOUR_STORAGE_ACCOUNT_NAME_HERE","$storName").Replace("YOUR_STORAGE_CONTAINER_NAME_HERE","$containerName").Replace("YOUR_STORAGE_ACCESS_KEY_HERE","$storKey")
-New-AzAPiManagement -ResourceGroupName $rgname -Name $apimName -location $location -organization $org -adminemail $email -Sku Consumption -Capacity 1
+New-AzAPiManagement -ResourceGroupName $rgname -Name $apimName -location $location -organization $org -adminemail $email -Sku Consumption
 
 $ApiMgmtContext = New-AzApiManagementContext -ResourceGroupName $rgName -ServiceName $apimName
-New-AzApiManagementApi -Context $ApiMgmtContext -Name $apiId -Protocols @("https") -Path "blob"
+$apiId = (New-AzApiManagementApi -Context $ApiMgmtContext -Name $apiName -Protocols @("https") -Path "blob" -serviceurl "https://api.contoso.com").apiId
 New-AzAPiManagementOperation -Context $ApiMgmtContext -ApiId $apiId -OperationId "get" -Name "GET" -method "GET" -UrlTemplate "/*"
-Set-AzApiManagementPolicy -Context $ApiMgmtContext -apiid $apiId -policy $Policy
+
+Set-Content -Path "policy.xml" -Value $Policy
+Set-AzApiManagementPolicy -Context $ApiMgmtContext -apiid $apiId -PolicyFilePath "policy.xml"
